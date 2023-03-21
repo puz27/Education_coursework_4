c061b1cb73e23371d427e2b55e40f2e46b588db2
anv <anv@woori.ru>
2023-03-21 11:33:38 +0300

    utils / add request to hh and sj

--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,48 @@
 import requests
 
-url_head_hunter = "https://api.hh.ru/vacancies"
-
-params = {
-    "text": "python",
-    "per_page": 5
-}
-
-# response = requests.get(url_head_hunter, params=params)
-# if response.status_code == 200:
-#     vacancies = response.json()["items"]
-#     for vacancy in vacancies:
-#          print(vacancy["name"], vacancy["url"], vacancy["area"]["name"], vacancy["salary"]["from"], vacancy["salary"]["to"])
-# else:
-#     print("Error:", response.status_code)
+#req = requests.get('https://api.hh.ru/employers', auth=('user', 'pass'))
+# print(req)
+# print(req.headers)
+# print(req.content)
+# Authorization: Bearer ACCESS_TOKEN
+# par = {'per_page': '10', 'page': 10}
+# #requests.get(self.url, params=par)
+# r = requests.get('https://api.hh.ru/employers', params=par)
+# print(r.content)
+# import requests
 
 
-url_super_job = "https://api.superjob.ru/2.0/vacancies/"
-params2 = {
-    "profession": "Старший специалист по тестированию",
-    "keys": "python",
-    "count": 10,
-    "page": 1
+# request to hh
+def get_vacancies_hh():
+    url_head_hunter = "https://api.hh.ru/vacancies"
+    params = {
+        "text": "python",
+        "per_page": 5
+    }
+    response = requests.get(url_head_hunter, params=params)
+    if response.status_code == 200:
+        vacancies = response.json()["items"]
+        for vacancy in vacancies:
+             print(vacancy["name"], vacancy["url"], vacancy["area"]["name"], vacancy["salary"]["from"], vacancy["salary"]["to"])
+    else:
+        print("Error:", response.status_code)
 
-}
 
-response = requests.get(url_super_job, params=params2)
-print(response.json())
+# request to superjob
+superjob_api_key = "v3.r.137436720.8c7f8aba97fa695bc8eb530e248876d05b91ac49.93eb1ab45d1127728b93d412484a5f08ae09f2c8"
+
+def get_vacancies_sj(api_key):
+    url_super_job = "https://api.superjob.ru/2.0/vacancies/"
+    headers = {'X-Api-App-Id': api_key}
+    params = {"keywords": "Python, python"}
+
+    response = requests.get(url_super_job, headers=headers, params=params)
+    if response.status_code == 200:
+        vacancies = response.json()["objects"]
+        for vacancy in vacancies:
+            print(vacancy["profession"], vacancy["town"]["title"])
+    else:
+        print("Error:", response.status_code)
+
+get_vacancies_sj(superjob_api_key)
+get_vacancies_hh()
