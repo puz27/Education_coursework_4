0004ccd1444b601bfb37461d4057672872fc5351
anv <anv@woori.ru>
2023-03-27 17:52:01 +0300

    add querys to src\utils

--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,32 @@
 import requests
 
-#req = requests.get('https://api.hh.ru/employers', auth=('user', 'pass'))
-# print(req)
-# print(req.headers)
-# print(req.content)
-# Authorization: Bearer ACCESS_TOKEN
-# par = {'per_page': '10', 'page': 10}
-# #requests.get(self.url, params=par)
-# r = requests.get('https://api.hh.ru/employers', params=par)
-# print(r.content)
-# import requests
+
+def requests_params_hh(name_vacancie: str, one_page_count: int = 5, all_page_count: int = 1):
+    params = {
+        "text": name_vacancie,
+        "per_page": one_page_count,
+        "page": all_page_count
+    }
+    return params
 
 
 # request to hh
-def get_vacancies_hh():
+def get_vacancies_hh(params: dict):
+    """
+    При указании параметров пагинации (page, per_page) работает ограничение: глубина возвращаемых результатов не может быть больше 2000.
+    Например, возможен запрос per_page=10&page=199 (выдача с 1991 по 2000 вакансию),
+    но запрос с per_page=10&page=200 вернёт ошибку (выдача с 2001 по 2010 вакансию)
+    :param params:
+    :return:
+    """
     url_head_hunter = "https://api.hh.ru/vacancies"
-    params = {
-        "text": "python",
-        "per_page": 5
-    }
     response = requests.get(url_head_hunter, params=params)
     if response.status_code == 200:
         vacancies = response.json()["items"]
         for vacancy in vacancies:
-             print(vacancy["name"], vacancy["url"], vacancy["area"]["name"], vacancy["salary"]["from"], vacancy["salary"]["to"])
+            return (print(vacancy["name"], vacancy["url"], vacancy["area"]["name"], vacancy["snippet"]))
+            #print(vacancy["name"], vacancy["url"], vacancy["area"]["name"], vacancy["snippet"]) # ,vacancy["salary"]["from"], vacancy["salary"]["to"] доделать проверку на ЗП
+
     else:
         print("Error:", response.status_code)
 
@@ -31,18 +34,33 @@ def get_vacancies_hh():
 # request to superjob
 superjob_api_key = "v3.r.137436720.8c7f8aba97fa695bc8eb530e248876d05b91ac49.93eb1ab45d1127728b93d412484a5f08ae09f2c8"
 
-def get_vacancies_sj(api_key):
+def requests_params_sj(name_vacancie: str):
+    #params = {"keywords": "Python, python"}
+    params = {"keywords": name_vacancie
+
+
+              }
+    return params
+
+def get_vacancies_sj(api_key: str, params: dict):
+    """
+    Прежде чем начать использовать API, необходимо зарегистрироваться и получить токен для работы,
+    более подробная инструкция описана по ссылке описания документации в разделе Getting started
+    :param api_key:
+    :param params:
+    :return:
+    """
     url_super_job = "https://api.superjob.ru/2.0/vacancies/"
     headers = {'X-Api-App-Id': api_key}
-    params = {"keywords": "Python, python"}
-
     response = requests.get(url_super_job, headers=headers, params=params)
     if response.status_code == 200:
         vacancies = response.json()["objects"]
         for vacancy in vacancies:
-            print(vacancy["profession"], vacancy["town"]["title"])
+            #print(vacancy)
+            print(vacancy["profession"],vacancy["profession"], vacancy["link"], vacancy["payment_from"], vacancy["payment_to"], vacancy["town"]["title"])
     else:
         print("Error:", response.status_code)
 
-get_vacancies_sj(superjob_api_key)
-get_vacancies_hh()
+
+get_vacancies_sj(superjob_api_key, requests_params_sj("python"))
+get_vacancies_hh(requests_params_hh("python"))
