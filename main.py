df0740f31fbfb396ce89f258c615586a29d65155
anv <anv@woori.ru>
2023-03-28 14:56:17 +0300

    add main metods to hh ans sj classes/test main

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.engine_classes import *
+from src.utils import *
+
+# x = HH(requests_params_hh("python"))
+# print(x.get_request(requests_params_hh("python")))
+# print(x.vacancies)
+
+
+y = SJ(superjob_api_key, requests_params_sj("python"))
+print(y.get_request(requests_params_sj("python")))
+print(y.vacancies)
+
--- src/engine_classes.py
+++ src/engine_classes.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
+import requests
 
 
 class Engine(ABC):
 
     @abstractmethod
-    def get_request(self):
-        """Return request"""
+    def get_request(self, *args, **kwargs):
         pass
 
     @staticmethod
@@ -16,11 +16,53 @@ class Engine(ABC):
 
 class HH(Engine):
     """Class for work with HeadHunter"""
-    def get_request(self):
-        pass
+
+    def __init__(self, params: str) -> None:
+        self.__vacancies = []
+        self.__params = params
+
+    @property
+    def vacancies(self):
+        return self.__vacancies
+
+    def get_request(self, params: dict) -> None:
+        """Return request"""
+        url_head_hunter = "https://api.hh.ru/vacancies"
+        response = requests.get(url_head_hunter, params=params)
+        if response.status_code == 200:
+            vacancies = response.json()["items"]
+            for vacancy in vacancies:
+                self.__vacancies.append((vacancy["name"], vacancy["url"], vacancy["area"]["name"], vacancy["snippet"]))
+                   # print(vacancy["name"], vacancy["url"], vacancy["area"]["name"], vacancy["snippet"]) # ,vacancy["salary"]["from"], vacancy["salary"]["to"] доделать проверку на ЗП
+        else:
+            return "Error:", response.status_code
+
 
 
 class SJ(Engine):
     """Class for work with SuperJob """
-    def get_request(self):
-        pass
+
+    def __init__(self, api_key: str, params: dict):
+        self.__vacancies = []
+        self.__params = params
+        self.__api_key = api_key
+
+    @property
+    def vacancies(self):
+        return self.__vacancies
+
+    def get_request(self, params):
+        url_super_job = "https://api.superjob.ru/2.0/vacancies/"
+        headers = {'X-Api-App-Id': self.__api_key}
+        response = requests.get(url_super_job, headers=headers, params=params)
+        if response.status_code == 200:
+            vacancies = response.json()["objects"]
+            for vacancy in vacancies:
+                self.__vacancies.append((vacancy["profession"], vacancy["profession"], vacancy["link"],
+                                          vacancy["payment_from"], vacancy["payment_to"], vacancy["town"]["title"]))
+
+        else:
+            return "Error:", response.status_code
+
+
+
